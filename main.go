211ad8f78f1d620de14450fd606fbfa6d0451bb6
Dev-HyunSang <me@hyunsang.dev>
2022-12-17 15:26:05 +0900

    feat: update create database schema

--- main.go
+++ main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"context"
+	"log"
+
+	"github.com/dev-hyunsang/clone-todo-mate/database"
 	"github.com/dev-hyunsang/clone-todo-mate/middleware"
 	"github.com/gofiber/fiber/v2"
-	"log"
 )
 
 func main() {
 	app := fiber.New()
 
 	middleware.Middleware(app)
+
+	client, err := database.ConnectionSQLite()
+	if err != nil {
+		log.Println("[ERROR] Failed to Connection DataBase")
+		log.Println(err)
+	}
+
+	defer client.Close()
+
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Println("[ERROR] Failed to Schema Create")
+		log.Println(err)
+	}
 
 	if err := app.Listen(":3000"); err != nil {
 		log.Println("[ERROR] Failed to Server Starting...")
