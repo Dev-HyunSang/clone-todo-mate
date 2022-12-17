485720e55b0ae8b8ceec513ab5ae9c67ef66039a
Dev-HyunSang <me@hyunsang.dev>
2022-12-17 15:24:45 +0900

    feat: update Middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,20 +1,22 @@
 package middleware
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/dev-hyunsang/clone-todo-mate/cmd"
+	"github.com/gofiber/fiber/v2"
+)
 
 func Middleware(app *fiber.App) {
 	api := app.Group("/api")
 
 	auth := api.Group("/auth")
-	auth.Post("/join")
-	auth.Post("/login")
-	auth.Post("/logout")
+	auth.Post("/join", cmd.JoinUser)
+	auth.Post("/login", cmd.LoginUser)
 
 	todo := api.Group("/todo")
-	todo.Post("/create")
-	todo.Post("/read")
-	todo.Post("/read/:uuid")
-	todo.Post("/update")
-	todo.Post("/completion")
-	todo.Delete("/delete/:uuid")
+	todo.Post("/create", cmd.CreaeteToDo)
+	todo.Get("/read", cmd.ReadToDo)
+	todo.Post("/edit", cmd.EditToDo)
+	todo.Post("/completion", cmd.CompletionToDo)
+	todo.Delete("/delete", cmd.DeleteToDo)
+	todo.Delete("/delete/:uuid", cmd.DeleteToDoParameter)
 }
